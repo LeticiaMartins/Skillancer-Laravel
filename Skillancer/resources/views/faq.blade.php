09f45e8dc0926c73c04d352301484a417d44486c
NataliaLira <natalia.rlira@gmail.com>
2018-11-21 02:25:05 -0200

    adicionando views e routes da faq, projetos e perfilDev

---
+++ Skillancer/resources/views/faq.blade.php
@@ -0,0 +1,308 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Skillancers</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+
+    <!-- Custom styles for this template-home -->
+    <link href="css/agency.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/agency.css">
+
+    <!-- Custom styles for this template-faq -->
+    <link rel="stylesheet" href="/css/faq.css">
+    <style>
+
+    img.logo{
+      width: 45px;
+      height:45px;
+    }
+
+    a{
+      color: antiquewhite;
+    }
+
+    img.big-logo{
+      width: 220px;
+      height: 220px;
+    }
+
+    div.intro-lead-in{
+      margin-top: 40px;
+    }
+
+    .carousel-inner{
+      position: relative;
+      margin-top: 2000px;
+      margin: 0 auto;
+      width: 600px;
+      height: 400px;
+    }
+
+    .textao{
+      positon: relative;
+      display: inline-block;
+    }
+
+    .freelas{
+      margin-top: 50px;
+      display: inline;
+    }
+
+    </style>
+
+  </head>
+
+  <body id="page-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top"><img class = "logo" src = "img/logos/logo.png" alt = "Skillancers"></a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          Menu
+          <i class="fas fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav text-uppercase ml-auto">
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#services">serviços</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#portfolio">projetos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#about">passo a passo</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="cadastro.php">cadastre-se</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="login.php">login</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Header -->
+    <header class="masthead">
+      <div class="container">
+        <div class="intro-text">
+          <!-- <div class= "logo-capa"><img class="big-logo" src = "img/logos/logo.png" alt = "Skillancers"></div> -->
+          <div class="intro-lead-in">Skillancers<img class="big-logo" src = "img/logos/logo.png" alt = "Skillancers"></div>
+          <div class="intro-heading text-uppercase">O FREELA IDEAL PARA VOCE</div>
+          <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">Cadastre-se</a>
+        </div>
+      </div>
+    </header>
+
+    <!-- F.A.Q. -->
+        <div class="container main">
+          <div class="col-md-4">
+            <ul class="list-group help-group">
+              <div class="faq-list list-group nav nav-tabs">
+                <a href="#tab1" id="a" class="list-group-item active" role="tab" data-toggle="tab">Perguntas Frequentes</a>
+                <a href="#tab2" id="b" class="list-group-item" role="tab" data-toggle="tab"><i class="mdi mdi-account"></i> Meu Perfil</a>
+                <a href="#tab3" id="c" class="list-group-item" role="tab" data-toggle="tab"><i class="mdi mdi-account-settings"></i> Minha Conta</a>
+                <a href="#tab4" id="d" class="list-group-item" role="tab" data-toggle="tab"><i class="mdi mdi-star"></i> Desenvolvedores</a>
+                <a href="#tab5" id="e" class="list-group-item" role="tab" data-toggle="tab"><i class="mdi mdi-cart"></i> Pagamento</a>
+              </div>
+            </ul>
+          </div>
+          <div class="col-md-8">
+            <div class="tab-content panels-faq">
+              <div class="tab-pane active" id="tab1">
+                <div class="panel-group" id="help-accordion-1">
+                  <div class="panel panel-help">
+                    <a href="#help-one" data-toggle="collapse" data-parent="#help-accordion-1">
+                      <div class="panel-heading" id="t1">
+                        <h2>Só uma empresa que se importa com o cliente criaria uma plataforma tão completa que responda todas suas perguntas</h2>
+                      </div>
+                    </a>
+                     <!-- <div id="help-one" class="collapse in"> -->
+                      <div class="panel-body">
+                      <img class="img-f" src="./image/acordo.jpg">
+                      </div>
+                    <!-- </div> -->
+                  </div>
+                </div>
+              </div>
+              <!-- 2 -->
+              <div class="tab-pane" id="tab2">
+                <div class="panel-group" id="help-accordion-2">
+                  <div class="panel panel-help">
+                    <a href="#help-two" data-toggle="collapse" data-parent="#help-accordion-2">
+                      <div class="panel-heading">
+                        <h2>Como alterar foto de perfil?</h2>
+                      </div>
+                    </a>
+                    <div id="help-two" class="collapse in">
+                      <div class="panel-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus nesciunt ut officiis accusantium quisquam minima praesentium, beatae fugit illo nobis fugiat adipisci quia distinctio repellat culpa saepe, optio aperiam est!</p>
+                        <p><strong>Example: </strong>Facere, id excepturi iusto aliquid beatae delectus nemo enim, ad saepe nam et.</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="panel panel-default panel-help">
+                    <a href="#help-two1" data-toggle="collapse" data-parent="#help-accordion-2">
+                      <div class="panel-heading">
+                        <h2>Onde configuro a privacidade do meu perfil?</h2>
+                      </div>
+                    </a>
+                    <div id="help-two1" class="collapse">
+                      <div class="panel-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus nesciunt ut officiis accusantium quisquam minima praesentium, beatae fugit illo nobis fugiat adipisci quia distinctio repellat culpa saepe, optio aperiam est!</p>
+                        <p><strong>Example: </strong>Facere, id excepturi iusto aliquid beatae delectus nemo enim, ad saepe nam et.</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="panel panel-default panel-help">
+                    <a href="#help-two2" data-toggle="collapse" data-parent="#help-accordion-2">
+                      <div class="panel-heading">
+                        <h2>Preciso cadastrar um novo e-mail.</h2>
+                      </div>
+                    </a>
+                    <div id="help-two2" class="collapse">
+                      <div class="panel-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus nesciunt ut officiis accusantium quisquam minima praesentium, beatae fugit illo nobis fugiat adipisci quia distinctio repellat culpa saepe, optio aperiam est!</p>
+                        <p><strong>Example: </strong>Facere, id excepturi iusto aliquid beatae delectus nemo enim, ad saepe nam et.</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!-- 3 -->
+              <div class="tab-pane" id="tab3">
+                <div class="panel-group" id="help-accordion-3">
+                  <div class="panel panel-default panel-help">
+                    <a href="#help-three" data-toggle="collapse" data-parent="#help-accordion-3">
+                      <div class="panel-heading">
+                        <h2>Como alterar minha senha?</h2>
+                      </div>
+                    </a>
+                    <div id="help-three" class="collapse">
+                      <div class="panel-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus nesciunt ut officiis accusantium quisquam minima praesentium, beatae fugit illo nobis fugiat adipisci quia distinctio repellat culpa saepe, optio aperiam est!</p>
+                        <p><strong>Example: </strong>Facere, id excepturi iusto aliquid beatae delectus nemo enim, ad saepe nam et.</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="panel panel-default panel-help">
+                    <a href="#help-three1" data-toggle="collapse" data-parent="#help-accordion-3">
+                      <div class="panel-heading">
+                        <h2>Cancelar Conta</h2>
+                      </div>
+                    </a>
+                    <div id="help-three1" class="collapse">
+                      <div class="panel-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus nesciunt ut officiis accusantium quisquam minima praesentium, beatae fugit illo nobis fugiat adipisci quia distinctio repellat culpa saepe, optio aperiam est!</p>
+                        <p><strong>Example: </strong>Facere, id excepturi iusto aliquid beatae delectus nemo enim, ad saepe nam et.</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!-- 4 -->
+              <div class="tab-pane" id="tab4">
+                <div class="panel-group" id="help-accordion-4">
+                  <div class="panel panel-default panel-help">
+                    <a href="#help-four" data-toggle="collapse" data-parent="#help-accordion-4">
+                      <div class="panel-heading">
+                        <h2>Como alterar minha senha?</h2>
+                      </div>
+                    </a>
+                    <div id="help-four" class="collapse">
+                      <div class="panel-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus nesciunt ut officiis accusantium quisquam minima praesentium, beatae fugit illo nobis fugiat adipisci quia distinctio repellat culpa saepe, optio aperiam est!</p>
+                        <p><strong>Example: </strong>Facere, id excepturi iusto aliquid beatae delectus nemo enim, ad saepe nam et.</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="panel panel-default panel-help">
+                    <a href="#help-four1" data-toggle="collapse" data-parent="#help-accordion-4">
+                      <div class="panel-heading">
+                        <h2>Onde encontro o portifólio?</h2>
+                      </div>
+                    </a>
+                    <div id="help-four1" class="collapse">
+                      <div class="panel-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus nesciunt ut officiis accusantium quisquam minima praesentium, beatae fugit illo nobis fugiat adipisci quia distinctio repellat culpa saepe, optio aperiam est!</p>
+                        <p><strong>Example: </strong>Facere, id excepturi iusto aliquid beatae delectus nemo enim, ad saepe nam et.</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!-- 5 -->
+              <div class="tab-pane" id="tab5">
+                <div class="panel-group" id="help-accordion-5">
+                  <div class="panel panel-default panel-help">
+                    <a href="#help-five" data-toggle="collapse" data-parent="#help-accordion-5">
+                      <div class="panel-heading">
+                        <h2>Quando o desenvolvedor disponibiliza o projeto após o pagamento?</h2>
+                      </div>
+                    </a>
+                    <div id="help-five" class="collapse">
+                      <div class="panel-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus nesciunt ut officiis accusantium quisquam minima praesentium, beatae fugit illo nobis fugiat adipisci quia distinctio repellat culpa saepe, optio aperiam est!</p>
+                        <p><strong>Example: </strong>Facere, id excepturi iusto aliquid beatae delectus nemo enim, ad saepe nam et.</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="panel panel-default panel-help">
+                    <a href="#help-five1" data-toggle="collapse" data-parent="#help-accordion-5">
+                      <div class="panel-heading">
+                        <h2>Quantos dias tenho para cancelar o pedido e ter o reembolso?</h2>
+                      </div>
+                    </a>
+                    <div id="help-five1" class="collapse">
+                      <div class="panel-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus nesciunt ut officiis accusantium quisquam minima praesentium, beatae fugit illo nobis fugiat adipisci quia distinctio repellat culpa saepe, optio aperiam est!</p>
+                        <p><strong>Example: </strong>Facere, id excepturi iusto aliquid beatae delectus nemo enim, ad saepe nam et.</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!--  -->
+            </div>
+          </div>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Contact form JavaScript -->
+    <script src="js/jqBootstrapValidation.js"></script>
+    <script src="js/contact_me.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="js/agency.min.js"></script>
+    <!-- Faq JavaScript -->
+    <script src="js/faq.js"></script>
+
+  </body>
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+</html>
