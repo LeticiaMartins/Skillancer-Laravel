1944b7357039475459ecf7e74e2187bc9db7c30e
leticia <lemartins.flag@gmail.com>
2018-11-27 20:38:54 -0200

    layout cadastro projetos

--- Skillancer/resources/views/projeto_todos.blade.php
+++ Skillancer/resources/views/projeto_todos.blade.php
@@ -3,7 +3,7 @@
 @section('css-custom', '/css/projetos.css')
 @section('content')
 
-<body>
+
   <h1>Lista de Projetos</h1>
   <h2><a href= "/projeto/adicionar">Adicionar Projeto</a></h2>
 
@@ -27,7 +27,7 @@
       <div class="container">
         <div class="row">
           <div class="col-lg-12 text-center">
-            <h2 class="section-heading text-uppercase">Projetos</h2>
+            <h2 class="section-heading text-uppercase">projetos</h2>
             <h3 class="section-subheading text-muted">Veja como a publicação de um projeto na Skillancers fala muito sobre seu potencial...depois disso é só escolher o/a desenvolverorxs ideal para você</h3>
           </div>
         </div>
@@ -54,12 +54,5 @@
           @endforeach
     </div>
     </section>
-      </body>
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
-    </html>
     @stop
