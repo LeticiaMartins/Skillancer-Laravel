2af9c00cecf9d089cd95f489cc05437fe0eb01dd
brunoalvesdasilvabr <brunoalvesdasilvabr@gmail.com>
2018-11-29 20:45:35 -0200

    não sei oq escrever

--- Skillancer/app/Http/Controllers/CadastroController.php
+++ Skillancer/app/Http/Controllers/CadastroController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App;
+use App\User;
+use App\Freelancer;
+use App\Habilidade;
 
 class Cadastro_Controller extends Controller
 {
@@ -17,4 +19,37 @@ class Cadastro_Controller extends Controller
 
         $user->save();
     }
+
+    public function cadastroFreelancer(){
+        $habilidades = Habilidade::all();
+
+        return view('cadastro_freelancer')->with('habilidades', $habilidades);
+    }
+
+    public function cadastroRetorno(Request $request){
+
+        $freelancer = Freelancer::create([
+            'fk_Id_user' =>  \Auth::user()->id,
+            'tipo_servico' => $request['tipo_servico'],
+            'linkedin' => $request['linkedin'],
+            'facebook' => $request['facebook'],
+            'twitter' => $request['twitter'],
+            'github' => $request['github']
+        ]);
+
+        $freelancer->save();
+
+        $habilidades = $request['habilidades'];
+        foreach($habilidades as $item){
+            $habilidade = Habilidade::create([
+                'Freelancer_id_freelancer' =>  $freelancer->id_freelancer,
+                'habilidade_id_habilidade' => $item
+            ]); 
+            $habilidade->save();
+        }
+
+        return [$freelancer, $habilidades];
+    }
+
+
 }
