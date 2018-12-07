4aab52daad1ee2be0b10cdbe2374781d310e7fcc
leticia <lemartins.flag@gmail.com>
2018-12-07 17:37:59 -0200

    layout projeto

--- Skillancer/resources/views/projeto_todos.blade.php
+++ Skillancer/resources/views/projeto_todos.blade.php
@@ -10,11 +10,8 @@
         <div class="row">
           <div class="col-lg-12 text-center">
             <h2 class="section-heading text-uppercase">projetos</h2>
-<<<<<<< HEAD
+
             <h4 class="section-subheading text-muted">Veja como a publicação de um projeto na Skillancers fala muito sobre seu potencial...depois disso é só escolher o/a desenvolverorxs ideal para você</h4>
-=======
-            <h3 class="section-subheading text-muted">Veja como a publicação de um projeto na Skillancers fala muito sobre seu potencial...depois disso é só escolher o/a desenvolvedor ideal para você</h3>
->>>>>>> f2c205bf00408a5061dde284dba39259b85f79b4
           </div>
         </div>
 
