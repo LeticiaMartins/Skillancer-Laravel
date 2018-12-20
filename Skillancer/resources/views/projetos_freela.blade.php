7ecea88e292d844361eba3aa26fea1d60323ee5e
leticia <lemartins.flag@gmail.com>
2018-12-20 17:14:58 -0200

    fonte pag projetos

--- Skillancer/resources/views/projetos_freela.blade.php
+++ Skillancer/resources/views/projetos_freela.blade.php
@@ -10,7 +10,7 @@
         <div class="row">
           <div class="col-lg-12 text-center">
             <h2 class="section-heading text-uppercase">projetos</h2>
-            <h3 class="section-subheading text-muted">Veja todos os projetos publicados e aplique para uma oportunidade.</h3>
+            <h2 class="section-subheading text-muted">Veja todos os projetos publicados e aplique para uma oportunidade.</h2>
           </div>
         </div>
 
